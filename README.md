3eb4dc95d943e8582bae0327d860598c3b0925f6
Sandra Szenti <alexandraszenti@gmail.com>
2014-01-17 12:53:07 -0500

    gh-pages setup with facebook alpha test

---
+++ README.md
@@ -0,0 +1,4 @@
+friendlocator
+=============
+
+mini project to gather all your facebook friends on a map based on their location
---
+++ js/main.js
@@ -0,0 +1,40 @@
+// function testAPI() {
+// 	console.log('Welcome!  Fetching your information.... ');
+// 	FB.api('/me', function(response) {
+// 		console.log('Good to see you, ' + response.name + '.');
+// 	});
+// }
+
+
+function initialize() {
+
+	// $.ajaxSetup({ cache: true });
+
+	// $.getScript('//connect.facebook.net/en_UK/all.js', function(){
+	// 	FB.init({
+	// 		appId: '587389484676369',
+	// 		status: true,
+	// 		cookie: true,
+	// 		xfbml: true
+	// 	});
+	// 	$('#loginbutton,#feedbutton').removeAttr('disabled');
+	// 	FB.getLoginStatus(updateStatusCallback);
+
+	// 	FB.Event.subscribe('auth.authResponseChange', function(response) {
+	// 		if (response.status === 'connected') {
+	// 			testAPI();
+	// 		}
+	// 		else if (response.status === 'not_authorized') {
+	// 			FB.login();
+	// 		}
+	// 		else {
+	// 			FB.login();
+	// 		};
+	// 	});
+	// });
+
+	
+}
+
+
+$(document).ready(initialize);
