c4498934dbdd6ae3546a765c363b1dc9c0b45199
Sandra Szenti <alexandraszenti@gmail.com>
2014-01-19 20:39:22 -0500

    fb load status and gmaps added to page, friends loaded

--- js/main.js
+++ js/main.js
@@ -3,11 +3,101 @@ var syncInit = false;
 var userLocationId;
 var userFBLoc;
 var friends = [];
+var friendsLocation = [];
 
-function initialize() {
+function mapLoad() {
+	$("#container").append("<div id='map-canvas'></div>");
+	console.log(userFBLoc);
+	var MY_MAPTYPE_ID = 'custom_style';
 
-	console.log("comment to check git origin master");
+	var featureOpts = [
+		{
+			"featureType": "water",
+			"stylers": [
+				{"visibility": "on" },
+				{ "color": "#003466" }
+			]
+		},
+		{
+			"featureType": "landscape",
+			"stylers": [
+				{ "visibility": "on" },
+				{ "color": "#ffffff" }
+			]
+		},
+		{
+			"featureType": "transit",
+			"stylers": [
+				{ "visibility": "on" },
+				{ "color": "#ffffff" }
+			]
+		},
+		{
+			"featureType": "road",
+			"stylers": [
+				{ "visibility": "on" },
+				{ "color": "#ffffff" }
+			]
+		},
+		{
+			"featureType": "administrative.province",
+			"stylers": [
+				{ "visibility": "on" },
+				{ "color": "#ffffff" }
+			]
+		},
+		{
+			"featureType": "administrative.land_parcel",
+			"stylers": [
+				{ "visibility": "on" },
+				{ "color": "#ffffff" }
+			]
+		},
+		{
+			"featureType": "poi",
+			"stylers": [
+				{ "visibility": "on" },
+				{ "color": "#ffffff" }
+			]
+		}
+	];
+	
+	var styledMapOptions = {
+		name: 'Custom Style'
+	};
 
+	var mapOptions = {
+		zoom: 5,
+		center: new google.maps.LatLng(userFBLoc.location.latitude,userFBLoc.location.longitude),
+		panControl: false,
+		zoomControl: false,
+		mapTypeControl: false,
+		scaleControl: false,
+		streetViewControl: false,
+		overviewMapControl: false,
+		disableDoubleClickZoom: false,
+		draggable: true,
+		scrollwheel: true,
+		mapTypeControlOptions: {
+			mapTypeIds: [
+				google.maps.MapTypeId.ROADMAP,
+				MY_MAPTYPE_ID
+			]
+		},
+		mapTypeId: MY_MAPTYPE_ID
+	};
+
+	var customMapType = new google.maps.StyledMapType(featureOpts, styledMapOptions);
+
+	map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+	map.mapTypes.set(MY_MAPTYPE_ID, customMapType);
+}
+
+function addFacebookStatusInfo () {
+	$("#container").append("<div id='facebook-load'>Thanks for trusting me with your data</div>");
+}
+
+function FBinit() {
 	window.fbAsyncInit = function() {
 		FB.init({
 			appId      : '198937526977949', // App ID
@@ -20,39 +110,46 @@ function initialize() {
 		syncInit = true;
 
 		if (syncInit) {
+			addFacebookStatusInfo();
 			$("#fb-login-button").click(
 				FB.login(function(response) {
 					if (response.authResponse) {
-						console.log('Welcome!  Fetching your information.... ');
+						$("#facebook-load").html("Welcome!  Fetching your information.... ");
+						
 						FB.api('/me', function(response) {
-							console.log(response);
-							console.log('Good to see you, ' + response.name + '.');
+							$("#facebook-load").html("Good to see you, " + response.name + "!")
 							var userId = response.id;
 							userLocationId = response.location.id;
 
-							console.log(userId);
-
-							FB.api("/"+userId+"/friends", function(response) {
-								console.log("Searching for friends...");
-								console.log(response);
-								friends = response.data();
-							});
-
 							FB.api("/"+userLocationId, function(response) {
 								userFBLoc=response;
+								console.log("userFBloc ", userFBLoc.location.latitude, userFBLoc.location.longitude)
+								mapLoad();
+
+							});
+							
+							FB.api("/"+userId+"/friends?fields=id,name,location", function(response) {
+								$("#facebook-load").html("Searching for friends...");
+								console.log(response);
+								friends = response.data;
+								$("#facebook-load").html(friends.length + " friends found on Facebook. Building your map now");
 							});
 
-							
 						});
+						
 						
 					}
 					else {
-						console.log('User cancelled login or did not fully authorize.');
+						$("#facebook-load").html("You cancelled login or did not fully authorize.");
 					};
 				})
 			);
-		}
+		}	
 	}
+}
+
+function initialize() {
+	FBinit();
 }
 
 
